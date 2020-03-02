7854b370f4a00c114b7a0331c33090f26cdeeb14
Felipe Lima <felipenplima@gmail.com>
2020-03-02 18:57:06 +0000

    new structure, lab finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Nav from './Nav';
+/* importar os componentes abaixo */
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Info from './components/Info';
 
 import './App.css';
 
@@ -8,22 +10,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Navbar />
+        <Main />
+        <Info />
       </div>
     );
   }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from ''./logo.svg';'
+import logo from './logo.svg';
 import ironhackLogo from './images/ironhack-logo.svg';
 import menu from './menu-top.svg';
 
